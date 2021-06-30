61ce290249216f0da691987dfd43b85363aa0782
Jon Estes <jlestes@gmail.com>
2021-06-29 21:52:41 -0500

    making ui changes
    this is to see the changes in chromatic

--- src/components/Task.js
+++ src/components/Task.js
@@ -17,7 +17,7 @@ export default function Task({
         />
         <span className="checkbox-custom" onClick={() => onArchiveTask(id)} />
       </label>
-      <div className="title">
+      <div className="title" style={{ fontSize: "2em" }}>
         <input
           type="text"
           value={title}
