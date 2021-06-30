148021acd1592d1e87bf525c81fc70d649682d5e
Jon Estes <jlestes@gmail.com>
2021-06-29 22:05:15 -0500

    remove style changes

--- src/components/TaskList.js
+++ src/components/TaskList.js
@@ -37,12 +37,7 @@ export function PureTaskList({ loading, tasks, onPinTask, onArchiveTask }) {
       <div className="list-items">
         <div className="wrapper-message">
           <span className="icon-check" />
-          <div
-            className="title-message"
-            style={{ fontSize: "2em", color: "deeppink" }}
-          >
-            You have no tasks
-          </div>
+          <div className="title-message">You have no tasks</div>
           <div className="subtitle-message">Sit back and relax</div>
         </div>
       </div>
